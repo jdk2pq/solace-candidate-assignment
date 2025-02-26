f934c5834162fba6822ab167589a0a80a487923b
Jake Kenneally <jakekenneally@gmail.com>
2025-02-26 17:15:38 -0500

    feat: add dark mode and factor out AdvocatesTableRow component
    - Added dark mode support using shadcn and Tailwind v4 to improve the UI, and it will use the user's system default
    - Factored out an `AdvocatesTableRow` component and made all phone numbers links that can be clicked using the `tel:` link type

--- src/components/AdvocatesTable.tsx
+++ src/components/AdvocatesTable.tsx
@@ -8,7 +8,7 @@ import {
 } from '@/components/ui/table'
 import { Advocate } from '@/types/advocate'
 import { SpinnerGap } from '@phosphor-icons/react'
-import { formatPhoneNumber } from 'react-phone-number-input'
+import { AdvocatesTableRow } from '@/components/AdvocatesTableRow'
 
 export function AdvocatesTable({ advocates = [], loading = false }: { advocates: Advocate[], loading?: boolean }) {
   return (
@@ -28,20 +28,9 @@ export function AdvocatesTable({ advocates = [], loading = false }: { advocates:
         {loading && <TableRow><TableCell><SpinnerGap size={32} className='animate-spin' /></TableCell></TableRow>}
         {!loading && advocates.length === 0 ?
           <TableRow><TableCell>No results found</TableCell></TableRow> : advocates.map((advocate) => {
-            return (
-              <TableRow key={advocate.firstName + advocate.lastName + advocate.phoneNumber}>
-                <TableCell>{advocate.firstName}</TableCell>
-                <TableCell>{advocate.lastName}</TableCell>
-                <TableCell>{advocate.city}</TableCell>
-                <TableCell>{advocate.degree}</TableCell>
-                <TableCell>
-                  {advocate.specialties.join(', ')}
-                </TableCell>
-                <TableCell>{advocate.yearsOfExperience}</TableCell>
-                <TableCell className="text-right">{formatPhoneNumber(`+1${advocate.phoneNumber}`)}</TableCell>
-              </TableRow>
+            return (<AdvocatesTableRow key={advocate.firstName + advocate.lastName + advocate.phoneNumber} advocate={advocate} />
             );
-        })}
+          })}
       </TableBody>
     </Table>
   )
